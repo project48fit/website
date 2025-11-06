5fa87a8695ad27770809e7785fc5642485c9e276
Birk Hanson <hanbi@MacBook-Pro-121.local>
2025-11-05 17:19:33 -0700

    chore: add google analytics tag

---
+++ src/pages/_document.tsx
@@ -0,0 +1,28 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const GA_MEASUREMENT_ID = 'G-DXBVPEHZYD';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_MEASUREMENT_ID}');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

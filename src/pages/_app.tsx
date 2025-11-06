341b2edd736ba1ab025cbd1c4199e5f50328ed44
Birk Hanson <hanbi@MacBook-Pro-121.local>
2025-11-06 00:14:02 -0700

    chore: set default social preview image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 import '../styles/theme.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://projectfitness.co';
+  const previewImage = `${siteUrl}/assets/images/1_logo.png`;
+
+  return (
+    <>
+      <Head>
+        <meta property="og:image" content={previewImage} />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={previewImage} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }

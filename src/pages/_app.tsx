e867991cb7d7686bad827600f92ee0d6b7b0f323
Birk Hanson <birkhanson@Birks-MacBook-Pro.local>
2026-02-14 01:44:03 -0700

    Install LinkedIn Insight Tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import dynamic from 'next/dynamic';
+import Script from 'next/script';
 import '../styles/globals.css';
 import '../styles/theme.css';
 
@@ -13,6 +14,33 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <LoaderOverlay />
+      <Script id="linkedin-insight-tag" strategy="afterInteractive">
+        {`
+_linkedin_partner_id = "9556041";
+window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+
+(function(l) {
+if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+window.lintrk.q=[]}
+var s = document.getElementsByTagName("script")[0];
+var b = document.createElement("script");
+b.type = "text/javascript";
+b.async = true;
+b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+s.parentNode.insertBefore(b, s);
+})(window.lintrk);
+`}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          alt=""
+          src="https://px.ads.linkedin.com/collect/?pid=9556041&fmt=gif"
+        />
+      </noscript>
       <Head>
         <meta property="og:image" content={previewImage} />
         <meta property="og:image:width" content="1200" />
